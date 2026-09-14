5b9c061a6638206130505e6f82eda47297424ab4
naroah <givefacegod@gmail.com>
2026-09-14 21:31:39 +0800

    fix: Disable power save and set bandwidth for WiFi connection

--- scanner/src/wifi.rs
+++ scanner/src/wifi.rs
@@ -18,12 +18,23 @@ pub fn connect(modem: Modem<'static>) -> Result<BlockingWifi<EspWifi<'static>>> 
 
     wifi.set_configuration(&wifi_configuration)?;
     wifi.start()?;
+    esp_idf_svc::sys::esp!(unsafe {
+        esp_idf_svc::sys::esp_wifi_set_ps(
+            esp_idf_svc::sys::wifi_ps_type_t_WIFI_PS_NONE,
+        )
+    })?;
+    esp_idf_svc::sys::esp!(unsafe {
+        esp_idf_svc::sys::esp_wifi_set_bandwidth(
+            esp_idf_svc::sys::wifi_interface_t_WIFI_IF_STA,
+            esp_idf_svc::sys::wifi_bandwidth_t_WIFI_BW_HT20,
+        )
+    })?;
     loop {
         match wifi.connect().and_then(|_| wifi.wait_netif_up()) {
             Ok(()) => break,
             Err(err) => {
                 log::warn!("WiFi connection failed: {:?}. Retrying...", err);
-                let _ = wifi.disconnect();
+                let _ = wifi.wifi_mut().disconnect();
                 std::thread::sleep(std::time::Duration::from_secs(2));
             }
         }
